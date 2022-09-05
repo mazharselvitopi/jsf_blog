495db7cb20abcd9095933898d4eb188d3867b0e4
mazharselvitopi <selvitopi_25@hotmail.com>
2022-09-05 23:44:07 +0300

    feature/first_branch - First commit

--- src/com/test/Bismillah.java
+++ src/com/test/Bismillah.java
@@ -9,8 +9,18 @@ import java.io.Serializable;
 @ViewScoped
 public class Bismillah implements Serializable {
     private final String BISMILLAH = "Bismillah her hayrın başıdır";
+    private String name ;
 
     public String getBISMILLAH() {
         return BISMILLAH;
+    }
+
+    public String getName() {
+        if (this.name == null || this.name.equals("")) return "Yahya ÖZTEMİR";
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
